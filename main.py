d14f3dd4c7d3319ed43ecb30b26a8b2923a1c557
Dinu Mihai <mihai.dinu93@gmail.com>
2023-09-25 20:49:08 +0300

    display all todos

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+todos = []
+
 def main_menu(program_name):
     print("*" * 50)
     print(program_name)
     print("*" * 50)
 
 
+def show_todos():
+    for i in todos:
+        print(i[0], i[1])
 
 main_menu("TODO APP")
